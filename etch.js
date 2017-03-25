1e5a3e2359af9153ee814c5321a154b0a2c583e2
Fwjchim <fwjchim.thaoxaochay@gmail.com>
2017-03-24 23:09:38 -0700

    Finally made the GRIDgit add .!

--- etch.js
+++ etch.js
@@ -1,13 +1,19 @@
 function drawCanvas(grid) {
-  var pixel = $("<div class='pixel' style='background:red'></div>");
-  var pixelHeight = $('#canvas').height() / grid;
-  var pixelWidth = $('#canvas').width() / grid;
+
+  var pixel = $("<div id='pixel'></div>");
+  var pixelHeight = (Math.floor((600 / grid)*100))/100;
+  var pixelWidth = (Math.floor((600 / grid)*100))/100;
   pixel.height(pixelHeight);
   pixel.width(pixelWidth);
 
-  for (var i=0; i<grid; i++) {
+
+
+
+  for (var i= (grid**2); i>0; i--) {
       $('#canvas').append(pixel.clone());
   }
+
+
 }
 
 
