c5e0def330a3c95e7095299b2537cbb4e20f8f6f
Fwjchim <fwjchim.thaoxaochay@gmail.com>
2017-03-26 23:41:47 -0700

    Color slection done.

--- etch.js
+++ etch.js
@@ -1,19 +1,14 @@
 function drawCanvas(grid) {
 
   var pixel = $("<div id='pixel'></div>");
-  var pixelHeight = (Math.floor((430 / grid)*100))/100;
+  var pixelHeight = (Math.floor((600 / grid)*100))/100;
   var pixelWidth = (Math.floor((600 / grid)*100))/100;
   pixel.height(pixelHeight);
   pixel.width(pixelWidth);
 
-
-
-
   for (var i= (grid**2); i>0; i--) {
       $('#canvas').append(pixel.clone());
   }
-
-
 }
 
 
@@ -24,7 +19,37 @@ $(document).ready(function() {
       drawCanvas(num)
     });
 
+    $(document).on("mouseover", "#pixel", function() {
+      if ($('#black').is(':checked')===true) {
+        $(this).css({"background-color":'black','opacity':1})
+      }
+      else if ($('#eraser').is(':checked')===true) {
+        $(this).css({"background-color":'white','opacity':0})
+      }
+      else if ($('#rainbow').is(':checked')===true) {
+        var r = Math.floor(Math.random() * 255 + 1);
+        var g = Math.floor(Math.random() * 255 + 1);
+        var b = Math.floor(Math.random() * 255 + 1);
+        var randomColor = "rgb(" + r + "," + g + "," + b +")";
+        $(this).css({"background-color":randomColor,'opacity':1})
+      }
+      else if ($('#pencil').is(':checked')===true) {
+        var opacity = parseFloat($(this).css('opacity'));
+					if (opacity === 0)
+					{
+						$(this).css({
+							'opacity': 0.1,
+							'background-color': 'black'
+						});
+					}
+					else
+					{
+						opacity += 0.1;
+						$(this).css({'opacity': opacity});
+					}
 
+      }
+    });
 
 
 
